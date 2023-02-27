65a4ca9eb635a4bd4d214f1a29fcec39513f2afb
xyfer17 <nav3044999@gmail.com>
2023-02-28 00:03:21 +0530

    add task commponent

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -8,10 +8,13 @@ const Home = () => {
       <Header />
       <Container>
         <Content>
-          <span>test</span>
-          <span>test</span>
-          <span>test</span>
-          <span>test</span>
+          <Section>
+            <span>Add the Tasks</span>
+            <Form>
+              <Input type="text" placeholder="Add the Task Here" />
+              <Button>Add</Button>
+            </Form>
+          </Section>
         </Content>
       </Container>
     </div>
@@ -30,5 +33,23 @@ const Content = styled.div`
   display: flex;
   flex-direction: column;
 `;
+
+const Section = styled.section`
+  background-color: white;
+  padding: 30px 120px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (max-width: 768px) {
+    padding: 50px;
+  }
+`;
+
+const Form = styled.form``;
+
+const Input = styled.input``;
+
+const Button = styled.button``;
 
 export default Home;
