bc7d2cfc04e5d9162277ae351d4d1c6debcc36e3
xyfer17 <nav3044999@gmail.com>
2023-03-01 21:37:44 +0530

    new update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -15,6 +15,26 @@ const Home = () => {
               <Button>Add</Button>
             </Form>
           </Section>
+
+          <DataItem>
+            <Item>
+              <span>test</span>
+              <button>submit</button>
+            </Item>
+
+            <Item>
+              <span>test</span>
+              <button>submit</button>
+            </Item>
+            <Item>
+              <span>test</span>
+              <button>submit</button>
+            </Item>
+            <Item>
+              <span>test</span>
+              <button>submit</button>
+            </Item>
+          </DataItem>
         </Content>
       </Container>
     </div>
@@ -31,25 +51,79 @@ const Container = styled.div`
 
 const Content = styled.div`
   display: flex;
+  align-items: center;
   flex-direction: column;
 `;
 
 const Section = styled.section`
   background-color: white;
-  padding: 30px 120px;
-  border-radius: 10px;
+  padding: 60px 180px;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.08);
   display: flex;
   flex-direction: column;
   align-items: center;
   @media (max-width: 768px) {
     padding: 50px;
   }
+
+  &:hover {
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+  }
+
+  span {
+    font-size: 20px;
+    font-weight: 500;
+    margin-bottom: 12px;
+    margin-top: -12px;
+  }
 `;
 
 const Form = styled.form``;
 
-const Input = styled.input``;
+const Input = styled.input`
+  width: 100%;
+  padding: 0.375rem 0.75rem;
+  border-radius: 0.25rem;
+  background: #fafbfc;
+  border-color: transparent;
+  border: 1px solid #dbe0ea;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  padding: 8px 15px;
+  border-radius: 0 7px 7px 0;
+  border: none;
+
+  z-index: 15;
+  margin-left: -1px;
+  background-color: #3455b8;
+  color: white;
+  &:hover {
+    background-color: #1d50e9;
+  }
+`;
+
+const Item = styled.div`
+  margin-top: 20px;
+  background-color: white;
+  padding: 60px 180px;
+  border-radius: 5px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.08);
+  display: flex;
+  justify-content: space-between;
+  @media (max-width: 768px) {
+    padding: 50px;
+  }
+  width: 100%;
+
+  &:hover {
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+  }
+`;
+
+const DataItem = styled.div`
+  margin-top: 90px;
+`;
 
 export default Home;
